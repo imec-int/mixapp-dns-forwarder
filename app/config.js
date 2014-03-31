16b4de60533c44abddf7f74454f9a1f09c791bb9
Sam <demo@iMac-1200282.local>
2014-03-31 16:35:58 +0200

    url updates

--- app/config.js
+++ app/config.js
@@ -4,9 +4,13 @@ exports.localip = ''; //leave empty for auto-detection
 exports.forwardaddress = '8.8.8.8'; // werkende DNS-server graag!
 
 exports.DNSentries = {
-	'www.mixapp.be' : '192.168.204.7',
-	'ab.mixapp.be' : '192.168.204.7',
-	'mixapp.be' : '192.168.204.7'
+	'www.mixapp.be' : '10.100.1.4',
+	'ab.mixapp.be' : '10.100.1.4',
+	'mixapp.be' : '10.100.1.4',
+	'hacklights.mixlab.be' : '10.100.1.4',
+	'mixmini.mixlab.be' : '10.100.1.2'
 }
 
 exports.logfile = 'log.txt';
+
+//iMac-1200282.local
