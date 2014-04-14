ab8a72f7bd2c6e033b45510dabf556930226f5ee
Sam Decrock <sam.decrock@gmail.com>
2014-04-14 14:51:40 +0200

    changed all ip's

--- app/config.js
+++ app/config.js
@@ -4,12 +4,14 @@ exports.localip = ''; //leave empty for auto-detection
 exports.forwardaddress = '8.8.8.8'; // werkende DNS-server graag!
 
 exports.DNSentries = {
-	'www.mixapp.be' : '10.100.1.4',
-	'ab.mixapp.be' : '10.100.1.4',
-	'mixapp.be' : '10.100.1.4',
-	'hacklights.mixlab.be' : '10.100.1.4',
-	'hacklights.mixapp.be' : '10.100.1.4',
-	'mixmini.mixlab.be' : '10.100.1.2'
+	'www.mixapp.be' : '10.100.1.1',
+	'ab.mixapp.be' : '10.100.1.1',
+	'mixapp.be' : '10.100.1.1',
+
+	'hacklights.mixlab.be' : '10.100.1.1',
+	'hacklights.mixapp.be' : '10.100.1.1',
+
+	'mixmini.mixlab.be' : '10.100.1.1'
 }
 
 exports.logfile = 'log.txt';
